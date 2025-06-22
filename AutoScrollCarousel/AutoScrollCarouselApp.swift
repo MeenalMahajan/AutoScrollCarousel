56b1c66a50f3d110a8856a6727d9e1b594908e82
Meenal Mahajan <meenalmahajan1912@gmail.com>
2025-06-22 17:16:44 +0530

    Initial Commit

---
+++ AutoScrollCarousel/AutoScrollCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoScrollCarouselApp.swift
+//  AutoScrollCarousel
+//
+//  Created by Meenal Mahajan on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoScrollCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
